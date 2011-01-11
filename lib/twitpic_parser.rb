3c781f9da448bbfb525f4ea6bc2ebc5ac3e81274
@edgarjs <edgar.js@gmail.com>
2011-01-11 15:38:30 -0600

    adding default extension for twitpic

--- lib/twitpic_parser.rb
+++ lib/twitpic_parser.rb
@@ -1,11 +1,11 @@
 class TwitpicParser
   
   def self.get_thumb_image(id)
-    "http://twitpic.com/show/thumb/#{id}"
+    "http://twitpic.com/show/thumb/#{id}.jpg"
   end
   
   def self.get_image(id)
-    "http://twitpic.com/show/large/#{id}"
+    "http://twitpic.com/show/large/#{id}.jpg"
   end
     
 end
